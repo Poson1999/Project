db795576e1deb1dae879f0f123d1582340a83489
hhakk0001 <myhanklin@gmail.com>
2021-12-21 18:45:20 +0800

    增加更改頭像的功能

--- all/lib/pages/edit_profile.dart
+++ all/lib/pages/edit_profile.dart
@@ -1,8 +1,58 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class EditProfile extends StatelessWidget {
   const EditProfile({Key? key}) : super(key: key);
+
+  Future<void> uploadpic() async{
+    XFile? file;
+    ImagePicker _picker = ImagePicker();
+    file = await _picker.pickImage(source: ImageSource.gallery);
+    String base64Image = "";
+
+    if(file == null){
+      // 如果沒有選中的圖片
+      Fluttertoast.showToast(
+        msg: "CanCelled",
+      );
+      return;
+    }else{
+      // 把圖片轉換成數值型態
+      File image = File(file.path);
+      List<int> imageBytes = image.readAsBytesSync();
+      base64Image = base64Encode(imageBytes);
+    }
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String userId = prefs.getString('UserId') ?? "null";
+
+    var url = "https://project-ccu-2021.000webhostapp.com/phpformobile/uploadpic.php";
+    var data = {
+      "image": base64Image,
+      "name": userId + ".png",
+    };
+
+    // 上傳並等待結果
+    try{
+      var res = await http.post(Uri.parse(url), body: data);
+      var result = jsonDecode(res.body);
+      Fluttertoast.showToast(
+        msg: result.toString(),
+      );
+
+    }catch(e){
+      Fluttertoast.showToast(
+        msg: e.toString(),
+      );
+    }
+
+
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -26,7 +76,9 @@ class EditProfile extends StatelessWidget {
         ListTile(
             title: const Text('Change Photo'),
             leading: const Icon(Icons.photo, color: Colors.green),
-            onTap: () { }
+            onTap: () {
+              uploadpic();
+            }
         ),
         const Divider(color: Colors.green),
         ListTile(
