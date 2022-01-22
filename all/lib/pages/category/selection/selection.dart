fccc7b7d8cbeea425904bf15ff46cd9d999085d9
hhakk0001 <myhanklin@gmail.com>
2022-01-22 23:03:00 +0800

    增加FAB,完成bookmark功能
    把FAB加入category的部分
    完成增加與轉跳書籤頁面的功能

---
+++ all/lib/pages/category/selection/selection.dart
@@ -0,0 +1,44 @@
+import 'package:all/pages/category/selection/selection_of_bamboo.dart';
+import 'package:all/pages/category/text.dart';
+import 'package:flutter/material.dart';
+
+class Selection extends StatelessWidget {
+  const Selection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(selectionTitle),
+        centerTitle: true,
+      ),
+      body: ListView(
+          children: <Widget> [
+            ListTile(
+              title: const Text(sob),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SelectionOfBamboo()));
+              },
+              trailing: const Icon(Icons.keyboard_arrow_right),
+            ),
+            const Divider(color: Colors.black45),
+            ListTile(
+              title: const Text(selectionTitle),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SelectionOfBamboo()));
+              },
+              trailing: const Icon(Icons.keyboard_arrow_right),
+            ),
+          ],
+      ),
+    );
+  }
+
+
+}
