1168dee4394a34b8dddf9b032532ff9ec681d0bd
hhakk0001 <myhanklin@gmail.com>
2022-01-23 20:23:48 +0800

    category 新增 & 確認格式
    新增Selection Of Bamboo
    作為初版格式
    之後再細調

--- all/lib/pages/constant.dart
+++ all/lib/pages/constant.dart
@@ -10,3 +10,17 @@ const Color authTextField = Colors.white38;
 
 const double defaultPadding = 16.0;
 const Duration defaultDuration = Duration(milliseconds: 300);
+
+// category 文字樣式
+
+const TextStyle titleStyle = TextStyle(
+  color: Color(0xFF306F33),
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+);
+
+const TextStyle textStyle = TextStyle(
+  color: Colors.black54,
+  fontSize: 17,
+  height: 1.35,
+);
