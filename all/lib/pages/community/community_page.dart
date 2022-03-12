4aa4805d671ff6d76b49f9c5c9217d145e5eb54c
hhakk0001 <myhanklin@gmail.com>
2022-03-12 22:51:46 +0800

    update post
    將post 移至資料庫

--- all/lib/pages/community/community_page.dart
+++ all/lib/pages/community/community_page.dart
@@ -1,13 +1,59 @@
+import 'package:all/pages/community/post.dart';
 import 'package:all/pages/community/posts.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
-class CommunityPage extends StatelessWidget {
+class CommunityPage extends StatefulWidget {
   const CommunityPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
+  _CommunityPageState createState() => _CommunityPageState();
+}
+
+class _CommunityPageState extends State<CommunityPage> {
+
+
+  List<Post> postList = <Post>[];
+
+  @override
+  void initState() {
+    getPostList();
+    super.initState();
+  }
+
+  void getPostList() async {
+    var url =
+        "https://project-ccu-2021.000webhostapp.com/phpformobile/getPost.php";
+
+    setState(() {
+      postList.clear();
+    });
+
+    try {
+      var res = await http.post(Uri.parse(url));
+      var jsonData = convert.jsonDecode(res.body);
+      for (var item in jsonData) {
+        setState(() {
+          postList.add(Post.fromJson(item));
+        });
+        debugPrint(item.toString());
+      }
+      debugPrint(postList.toString());
+    } catch (e) {
+      debugPrint(e.toString());
+      Fluttertoast.showToast(
+        msg: "Error: " + e.toString(),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         body: ListView.builder(
-            itemCount: posts.length,
+            itemCount: postList.length,
             itemBuilder: (context, index) => Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
@@ -15,13 +61,13 @@ class CommunityPage extends StatelessWidget {
                           padding: const EdgeInsets.symmetric(vertical: 10),
                           child: ListTile(
                               leading: ClipOval(
-                                  child: Image.network(posts[index].authorPic,
+                                  child: Image.network(postList[index].authorPic,
                                       fit: BoxFit.cover)),
-                              title: Text(posts[index].authorName, style: author),
-                          trailing: Text(posts[index].time, style: time))),
+                              title: Text(postList[index].authorName, style: author),
+                          trailing: Text(postList[index].time, style: time))),
                       Padding(
                           padding: const EdgeInsets.only(left: 10, right: 10),
-                          child: Text(posts[index].content, style: content)),
+                          child: Text(postList[index].content, style: content)),
                       Padding(
                           padding: const EdgeInsets.all(10),
                           child: SizedBox(
@@ -32,4 +78,5 @@ class CommunityPage extends StatelessWidget {
                       const Divider(height: 3, color: Colors.black)
                     ])),
       );
+  }
 }
--- all/lib/pages/community/post.dart
+++ all/lib/pages/community/post.dart
@@ -2,4 +2,19 @@ class Post {
   String content, authorPic, authorName, picture, time;
 
   Post(this.content, this.authorPic, this.authorName, this.picture, this.time);
+
+  Post.fromJson(Map<String, dynamic> json)
+      : content = json['content'],
+        authorPic = json['photo'],
+        authorName = json['name'],
+        picture = json['picture'],
+        time = json['post_time'];
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+    'content': content,
+    'photo': authorPic,
+    'name': authorName,
+    'picture': picture,
+    'post_time': time,
+  };
 }
